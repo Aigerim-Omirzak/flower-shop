36cff1dd6e717b1f0991abb53d7e2c7aeb182363
Aigerim-Omirzak <74128426+Aigerim-Omirzak@users.noreply.github.com>
2023-10-24 01:21:07 +0600

    Update hello.js

---
+++ hello.js
@@ -0,0 +1,34 @@
+function validateForm() {
+    let name = document.forms["myForm"]["fname"].value;
+    let email = document.forms["myForm"]["email"].value;
+    let address = document.forms["myForm"]["address"].value;
+
+    // Check if the Name field is empty
+    if (name === "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    // Check if the Email field is empty and validate the email format
+    if (email === "") {
+        alert("Email must be filled out");
+        return false;
+    } else if (!validateEmail(email)) {
+        alert("Invalid email address");
+        return false;
+    }
+
+    // Check if the Address field is empty
+    if (address === "") {
+        alert("Address must be filled out");
+        return false;
+    }
+
+    return true;
+}
+
+// Function to validate email format
+function validateEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+}
